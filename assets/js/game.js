4c5995b13641b394399cc9775b8715c3fbdf680d
Chris Pietrangeli <chris.pietrangeli11@gmail.com>
2022-01-11 11:34:01 -0700

    mvp

--- assets/js/game.js
+++ assets/js/game.js
@@ -74,16 +74,54 @@ var fight = function(enemyName) {
     }
 };
 
-for (var i = 0; i < enemyNames.length; i++) {
+//function to start a game
+var startGame = function() {
+
+    //reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
+    for (var i = 0; i < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1) );
+        
+            var pickedEnemyName = enemyNames[i];
+            enemyHealth = 50;
+            fight(pickedEnemyName);
+        }
+        else {
+            window.alert('You have lost your robot in battle! Game Over!');
+            break;
+        }
+    }
+    // after the loop ends, player is either out of health or the enemies to fight, so run the endGame function
+    endGame();
+};
+
+//function to end entire game
+var endGame = function() {
+    //if player is still alive, Player wins!
     if (playerHealth > 0) {
-        window.alert("Welcome to Robot Gladiators! Round " + (i + 1) );
-    
-        var pickedEnemyName = enemyNames[i];
-        enemyHealth = 50;
-        fight(pickedEnemyName);
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + " . ");
     }
     else {
-        window.alert('You have lost your robot in battle! Game Over!');
-        break;
+        window.alert("You've lost your robot in battle.");
     }
-}
+
+    window.alert("The game has now ended. Let's see how you did!");
+
+    //ask player if they'd like to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+        //restart game
+        startGame();
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+};
+
+//start the game when the page loads
+startGame();
